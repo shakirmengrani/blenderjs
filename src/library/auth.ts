29e0b7bb59897becd66dcc3e71c80fc85d44a5b2
Shakir Mengrani <shakir.mengrani@experia.ai>
2020-10-23 18:39:12 +0500

    auth for generate token

---
+++ src/library/auth.ts
@@ -0,0 +1,27 @@
+import { JWT, JWK } from 'jose'
+import * as jsonwebtoken from 'jsonwebtoken'
+import {config as AppConfig} from '../config/app'
+
+
+export const generateKey = (payload: object) => {
+    payload["iat"] = Date.now()
+    return JWT.sign(payload, JWK.asKey(AppConfig.openid.jwt.secretOrKey), {audience: AppConfig.openid.jwt.audience, expiresIn: "1d", algorithm: "HS256"});
+}
+
+export const verify = (token: string = null) => {
+    return JWT.verify(token, JWK.asKey(AppConfig.openid.jwt.secretOrKey))
+}
+
+export const issueToken = (user: any, expiresIn: string=AppConfig.openid.jwt.expiresIn) => {
+    const payload = {
+        user, "iat": Date.now(), "aud": AppConfig.openid.jwt.audience
+    } 
+    const token = jsonwebtoken.sign({user}, AppConfig.openid.jwt.secretOrKey, {
+        algorithm: 'HS256',
+        audience: AppConfig.openid.jwt.audience,
+        expiresIn
+    })
+    return {
+        token
+    }
+}
